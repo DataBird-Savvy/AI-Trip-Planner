582fbc472f5887c665b0104aae8f7ead8b823056
jiyamary <mejiyamaryjoseph@gmail.com>
2026-09-26 15:13:55 +0530

    model loader finished

---
+++ agent/agentic_workflow.py
@@ -0,0 +1,19 @@
+from  utils.model_loaders import load_model
+from prompt_library.prompt import SYSTEM_PROMPT
+from langgraph.graph import StateGraph, MessagesState, END, START
+from langgraph.prebuilt import ToolNode, tools_condition
+
+
+class Graphbuilder:
+    def __init__(self):
+        pass
+
+
+    def agent_function(self):
+        pass
+
+    def build_graph(self):
+        pass 
+
+    def __call__(self):
+        pass
